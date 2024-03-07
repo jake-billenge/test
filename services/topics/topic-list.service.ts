e17284b3f76a6907a4778ce26cefb62240543219
jklee <136395965+jake-billenge@users.noreply.github.com>
2024-03-07 11:48:51 +0900

    Update topic-list.service.ts

--- services/topics/topic-list.service.ts
+++ services/topics/topic-list.service.ts
@@ -1,7 +1,7 @@
 import { AbstractTopicService } from '@src/topics/services/topics/abstract-topic.service';
 import { Injectable } from '@nestjs/common';
 
-@Injectable()
+@Injectable({ scope: Scope.REQUEST })
 export class TopicListService extends AbstractTopicService {
     public constructor() {
         super();
